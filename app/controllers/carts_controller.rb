2a983c2861759ef19e8660f8cd658b2a82932f9d
Qiang Guo <qiang.guo@gettyimages.com>
2017-02-15 16:56:57 -0700

    Not work much

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,20 +9,20 @@ class CartsController < ApplicationController
   
   def update
     cart = get_cart
-    cart_original = cart
     begin
       params[:cart_items].each do |it|
         cart.apply(it)
       end
       head :ok
     rescue Exceptions::CartError
-      render :json => {error: 'could not update cart', items: cart_original}, :status => 500
+      render :json => {error: 'could not update cart'}, :status => 500
     end
   end
 
   private
 
   def get_cart
+    byebug
     session['cart'] ||= Cart.new
     return session['cart']
   end
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -7,6 +7,16 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     @product2 = products(:cream)
   end
 
+  ['add', 'del'].each do |op|
+    define_method(op + '_products') do |items|
+      items = if items.kind_of?(Array) then items else [items] end
+      put cart_path, xhr: true, params: {:cart_items => items.map { |x| {:op => op == 'add' ? '+' : '-', :product => x } }}
+    end
+  end
+
+  def assert_cart_size(expected)
+    assert_equal(expected, @request.session['cart'].size)
+  end
 
   test "return empty cart initially" do
     get cart_path, xhr: true
@@ -27,6 +37,17 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     put cart_path, xhr: true, params: { :cart_items => [{:op => '-', :product => @product1.sku}] }
     assert_response(:error)
     assert_match('could not update cart', JSON.parse(response.body)["error"])
-    assert_empty(JSON.parse(response.body)["items"])
+    # make sure cart is not altered
+    assert_cart_size(0)
+  end
+
+  test "update multiple times" do
+    add_products [@product1.sku, @product1.sku, @product2.sku]
+    assert_response(:success)
+    assert_cart_size(3)
+
+    del_products [@product1.sku]
+    assert_response(:success)
+    assert_cart_size(1)
   end
 end
