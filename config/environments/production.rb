98792b51e9be6ec70776c2467c8c1d2e17fab452
Qiang Guo <qiang.guo@gettyimages.com>
2017-04-05 16:42:23 -0600

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'superunique.ca' }
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.smtp_settings = {
     address: 'mail.hover.com',
