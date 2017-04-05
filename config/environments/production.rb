9f3046eb4a792347a086933ecfaac9a3b455be14
Qiang Guo <qiang.guo@gettyimages.com>
2017-04-05 15:23:17 -0600

    production email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dune_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'superunique.ca' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
