0ce7b7e03a113a17870a7663dfef1b294288adc7
Qiang Guo <mcknight0219@gmail.com>
2017-05-02 20:17:21 -0600

    remove browserify config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.browserify_rails.node_env = 'production'
 end
